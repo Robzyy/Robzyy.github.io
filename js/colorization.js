1d829140a633ae61cea1a664afd3c117596f4854
Robzyy <cclipicioiu@gmail.com>
2023-08-16 13:21:40 +0300

    Just look at the diff

--- js/colorization.js
+++ js/colorization.js
@@ -1,39 +1,49 @@
 "use strict";
 
+// ✏️ TODO: Unrandomize lol
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
 
-const thing = [
+const array = [
     {
-        img: "/assets/me2.jpg",
         palette_file: "css/color-tokens_blue.css",
+        hero_img: "/assets/me2.jpg",
+        hero_img_desc: null
+
     },
     {
-        img: "https://source.unsplash.com/cVJsVGkxCoo.png",
         palette_file: "css/color-tokens_yellow.css",
+        hero_img: "https://source.unsplash.com/cVJsVGkxCoo.png",
+        hero_img_desc: "Image from Unsplash"
     },
     {
-        img: "/assets/strokes1.jpg",
         palette_file: "css/color-tokens_purple.css",
+        hero_img: "/assets/strokes1.jpg",
+        hero_img_desc: "Unknown image source..."
     },
 ]
 
-let x = getRandomInt(thing.length)
+let x = getRandomInt(array.length)
 
-// Get HTML head element
+// Include the CSS color palette in the <head> element
 let head = document.getElementsByTagName('HEAD')[0];
-
-// Create new link Element
 let link = document.createElement('link');
-
-// set the attributes for link element
 link.rel = 'stylesheet';
 link.type = 'text/css';
-link.href = thing[x].palette_file;
-
-// Append link element to HTML head
+link.href = array[x].palette_file;
 head.appendChild(link);
 
+// Replace the hero image
 let img = document.getElementById("hero-img");
-img.src = thing[x].img;
+img.src = array[x].hero_img;
+
+// Replace the hero image description
+let info = document.getElementById("hero-image-info");
+if (array[x].hero_img_desc == null) {
+    info.style.display = "none"
+}
+else {
+    info.childNodes[3].innerHTML = array[x].hero_img_desc
+}
+
